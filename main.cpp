cdccfec4e85a96d98d733e37a004a46395d6772c
Pedro Augusto Serafim Belo <79765850+PedroASB@users.noreply.github.com>
2021-11-04 16:03:57 -0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,34 @@
+// Código de exemplo de execução do algoritmo de Kruskal
+
+#include <iostream>
+#include "disjointSet.hpp"
+#include "kruskal.hpp"
+
+using namespace std;
+
+int main(){
+	int qtt_vertices = 5;
+	Graph graph(qtt_vertices);
+	Graph minimumGraph;
+
+	graph.insert(4, 1, 2);
+	graph.insert(1, 0, 4);
+	graph.insert(5, 2, 4);
+	graph.insert(1, 1, 4);
+	graph.insert(2, 0, 1);
+	graph.insert(1, 2, 3);
+
+	cout << "(W: <peso> | <vertice 1> - <vertice 2>)" << endl;
+
+	cout << "\nGrafo Original:" << endl;
+	graph.print();
+	cout << "Peso Total: " << graph.getWeight() <<endl;
+
+	minimumGraph = graph.kruskal();
+	
+	cout << "\nGrafo Minimo:" << endl;
+	minimumGraph.print();
+	cout << "Peso Total: " << minimumGraph.getWeight() <<endl;
+
+	return 0;
+}
